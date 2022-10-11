fc2e9338e1077e60933fac6100d1fff9c60de22a
Aajcm <115163957+Aajcm@users.noreply.github.com>
2022-10-11 20:44:02 +0800

    Update Demo1.java

--- src/main/java/gittest/Demo1.java
+++ src/main/java/gittest/Demo1.java
@@ -9,6 +9,6 @@ public class Demo1 {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
-        System.ou.println("pull test");
+        System.out.println("pull test");
     }
 }
